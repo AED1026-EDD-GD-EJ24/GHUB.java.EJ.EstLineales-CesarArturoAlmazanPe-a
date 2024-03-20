b8b877bc3a408276c4288b826ca67d2245d094c1
Cesar Arturo Almazan Peña <146864350+CesarAlmazan@users.noreply.github.com>
2024-03-20 09:16:48 -0700

    Initial commit

---
+++ miPrincipal/Principal.java
@@ -0,0 +1,51 @@
+package miPrincipal;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+        int opc =0;
+        do{
+            System.out.println("*************************");
+            System.out.println("  Estructuras Lineales   ");
+            System.out.println("*************************");
+            System.out.println(" 1) Pila");
+            System.out.println(" 2) Stack");
+            System.out.println(" 3) Delimitadores");
+            System.out.println("4) Expresion Infija-Prefija");
+            System.out.println("5) Cola");
+
+            System.out.println();
+
+            System.out.println("0) SALIR");
+            System.out.print("Selecciona opción:");
+            opc=consola.nextInt();
+            switch (opc){
+                case 1:
+                   DemoPila.menu();
+                   break;
+                case 2:
+                   DemoStack.menu();
+                   break;
+                case 3:
+                   DemoDelimitadores.menu();
+                   break;
+                case 4:
+                   //tarea
+                case 5:
+                   DemoCola.menu();
+                   break;
+                case 0:
+                    System.out.println("ADIOS!");
+                    consola.close();
+                    break;
+                default:
+                    System.out.println("Valor incorrecto, intente de nuevo!");
+            }
+
+        } while (opc != 0);
+
+    }
+    
+}
