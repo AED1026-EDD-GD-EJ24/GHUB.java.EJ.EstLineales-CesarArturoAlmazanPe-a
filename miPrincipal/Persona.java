e36da9d0ca25bf22e0984491165f4e07f78ab199
Cesar Arturo Almazan Peña <146864350+CesarAlmazan@users.noreply.github.com>
2024-03-21 16:37:38 +0000

    21/03/24

---
+++ miPrincipal/Persona.java
@@ -0,0 +1,16 @@
+package miPrincipal;
+
+public class Persona {
+    Private String nombre;
+    private int edad;
+    private String Nacionalidad;
+    pblic Persona(String nombre, int edad, String nacionalidad){
+        this.nombre = nombre;
+        this.edad = edad;
+        nacionalidad = nacionalidda;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+}
